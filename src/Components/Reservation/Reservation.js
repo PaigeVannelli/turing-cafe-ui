c108698e1aef7b203d4596b0032c67e77698bc34
Paige Vannelli <paigevannelli@gmail.com>
2021-04-07 08:36:15 -0700

    Style reservation cards

--- src/Components/Reservation/Reservation.js
+++ src/Components/Reservation/Reservation.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Reservation.css'
 
 const Reservation = ({name, date, time, number}) => {
 
     return (
-        <article>
+        <article className='reservation-card'>
             <h1>{name}</h1>
             <p>{date}</p>
             <p>{time} pm</p>
